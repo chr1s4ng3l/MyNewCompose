813057163ddf452980a9d98b0e0f89c15248c787
Consultant <christopher.tamayocazarez@gmail.com>
2023-02-27 20:15:23 -0500

    Initial commit compose

---
+++ app/src/main/java/com/example/mynewcompose/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.mynewcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Device
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.mynewcompose.ui.theme.MyNewComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyNewComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hi mi name is $name", modifier = Modifier
+        .height(20.dp)
+        .fillMaxWidth()
+        .size(30.dp))
+}
+
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    device = Devices.DEFAULT,
+    apiLevel = 33
+)
+@Composable
+fun DefaultPreview() {
+    MyNewComposeTheme {
+        Greeting("Android")
+    }
+}
